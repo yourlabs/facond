7c4f4fe5e3d82ac116890c086a9eb266b45cd297
jpic <jamespic@gmail.com>
2016-10-06 13:30:19 +0200

    Fix ImportError: No module named settings

--- test_ddf_project/wsgi.py
+++ test_ddf_project/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__)))
+)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
