2284d241235205abe9f732a8e5c5daa4bf992d4f
jpic <jamespic@gmail.com>
2018-03-01 16:42:23 +0100

    wip

--- src/ddf/action.js
+++ src/ddf/action.js
@@ -1,10 +1,20 @@
 class Action {
   constructor(conditions) {
-    this.conditions = conditions
+    this.conditions = conditions || []
   }
 
   apply(form, field) {
-    console.log('apply()', form, field)
+    let method = 'apply';
+
+    for (var i=0; i<this.conditions.length; i++) {
+        if (!this.conditions[i].validate(form)) {
+            method = 'unapply';
+            break;
+        }
+    }
+
+    if (ddf.debug) console.log('[Action] ', this, '.', method, '(', form, ',', field, ')');
+    this[method](form, field);
   }
 }
 
--- src/ddf/index.js
+++ src/ddf/index.js
@@ -1,7 +1,52 @@
-import { action } from './action'
+import $ from "jquery"
+
+const ddf = {
+  instanciate: function(attrs) {
+    let cls = ddf.getClass(attrs.cls);
+    let obj = new cls();
+
+    for (key in attrs) {
+        let value = attrs[key];
+
+        if (attrs[key] === undefined || attrs[key] === null) {
+            obj[key] = value;
+        } else if (value instanceof Array && value.length && value[0].cls !== undefined) {
+            obj[key] = value.map(ddf.instanciate);
+        } else if (attrs[key].cls !== undefined) {
+            obj[key] = ddf.instanciate(attrs);
+        } else {
+            obj[key] = value;
+        }
+    }
+
+    if (ddf.debug) console.log('Instanciated', obj);
+    return obj;
+  }
+  getClass: function(name) {
+    let parts = name.split('.');
+    let leaf = window;
+
+    for(var i=0; i<parts.length; i++) {
+        leaf = leaf[parts[i]];
+    }
+
+    if (ddf.debug) console.log('Found', leaf, 'for', name);
+    return leaf
+  },
+}
+
+$(document).ready(function() {
+  $.fn.ddf = function(configuration) {
+    let form = ddf.instanciate(configuration);
+    form.bind($(this));
+    form.update();
+  }
+
+  $('script[type="text/ddf-configuration"]').each(function() {
+    $(this).parents('form').ddf(JSON.parse($(this).text()));
+  });
+})
 
 export default {
-  ddf: {
-    actions: actions
-  }
+  ddf: ddf
 }
