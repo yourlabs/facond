4b12aba6a057e626041209c5c0db76f9c89b7417
jpic <jamespic@gmail.com>
2018-03-02 00:00:18 +0100

    wip

---
+++ src/ddf/form.test.js
@@ -0,0 +1,21 @@
+import { JSDOM } from 'jsdom'
+import $ from 'jquery'
+import { Form } from './form'
+
+const formElement = () => new JSDOM(`
+<html><body>
+<form>
+  <div>
+   <input id="id_name" name="name" />
+   <label for="id_name">Name</label>
+  </div>
+</body></html>
+`).querySelector('form')
+
+describe('Form', () => {
+  test('should bind form', () => {
+    var subject = new Form([], 'bar')
+    subject.bind(formElement)
+    expect(subject.form).toBe(formElement)
+  })
+})
